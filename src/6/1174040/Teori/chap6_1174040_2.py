59b7ea36c2924a6b28e666dec85bc26df080c014
haganz753 <aduhsakit753@gmail.com>
2019-04-09 12:54:56 +0700

    chap6 hagan

---
+++ src/6/1174040/Teori/chap6_1174040_2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 03:43:07 2019
+
+@author: Haganz
+"""
+
+import matplotlib.pyplot as plt
+x = (4,8,13,17,20)
+y = (54, 67, 98, 78, 45)
+plt.plot(x,y)
+plt.show()
