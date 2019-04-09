15c92a93fc3aec9448f0118dff78c857825cb0dd
dikasukmap <dikasukma40@gmail.com>
2019-04-09 23:26:12 +0700

    d1ka

---
+++ src/6/1174050/Teori/subplot.py
@@ -0,0 +1,33 @@
+#subplot
+import matplotlib.pyplot as plt
+
+plt.subplot(331)
+x = (4,8,12,16,20)
+y = (54, 58, 62, 66, 70)
+
+plt.plot(x,y)
+plt.subplot(332)
+
+plt.plot(x,y)
+plt.subplot(333)
+
+plt.plot(x,y)
+plt.subplot(334)
+
+plt.plot(x,y)
+plt.subplot(335)
+
+plt.plot(x,y)
+plt.subplot(336)
+
+plt.plot(x,y)
+plt.subplot(337)
+
+plt.plot(x,y)
+plt.subplot(338)
+
+plt.plot(x,y)
+plt.subplot(339)
+
+plt.plot(x,y)
+plt.show()
