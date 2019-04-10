0ba13e02ae7040b50f57f25f10cb41d27f11473b
ichsanhizmanhardy <ichsanhh13@gmail.com>
2019-04-10 23:12:51 +0700

    ichsan

---
+++ src/6/1174034/Praktek/main_san.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 22:27:28 2019
+
+@author: Ichsan Hizman
+"""
+
+import p1174034_bar as bar
+import p1174034_titik as sct
+import p1174034_pie as pie
+import p1174034_plot as plt
+
+bar.balok()
+sct.sebaran()
+pie.pie_chart()
+plt.plot()
