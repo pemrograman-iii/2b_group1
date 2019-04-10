d61341565c9f2225e5e440dbaf86f0e604d7803f
sspitzenreiter <luthfimnabil17@gmail.com>
2019-04-10 16:02:12 +0700

    Push master

---
+++ src/6/1174035/Praktek/chap6_1174035_bar.py
@@ -0,0 +1,20 @@
+def BarPanggil(npm):
+	import matplotlib.pyplot as plt
+	import random as rand
+	i = (int(npm)%3)+2
+	rowcol = ""
+	if i%2==0:
+		row=i/2
+		rowcol=str(int(row))+"2"
+	if i%3==0:
+		row=i/3
+		rowcol=str(int(row))+"3"
+	a=1	
+	while a<=i:
+		y = [rand.randint(1,10),rand.randint(1,10),rand.randint(1,10),rand.randint(1,10)]
+		plt.subplot(int(rowcol+str(a)))
+		plt.bar([1,2,3,4], y)
+		a+=1	
+	plt.show()
+	return i
+BarPanggil("1174035")
